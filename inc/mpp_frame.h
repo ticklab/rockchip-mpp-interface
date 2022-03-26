e8713d0b35c5cd4f6297e356e23609053daf37a5
Johnson Ding <johnson.ding@rock-chips.com>
2022-03-26 18:06:22 +0800

    [mpp]: sync header definition with kmpp branch
    Signed-off-by: Johnson Ding <johnson.ding@rock-chips.com>
    Change-Id: Ic586bb0e54b5f270d504bc72f2aeb0648a207cbf

--- inc/mpp_frame.h
+++ inc/mpp_frame.h
@@ -221,6 +221,8 @@ typedef enum {
     MPP_FMT_YUV440SP        = (MPP_FRAME_FMT_YUV + 13), /* YYYY... UVUV...          */
     MPP_FMT_YUV411SP        = (MPP_FRAME_FMT_YUV + 14), /* YYYY... UV...            */
     MPP_FMT_YUV444SP        = (MPP_FRAME_FMT_YUV + 15), /* YYYY... UVUVUVUV...      */
+    MPP_FMT_YUV444P         = (MPP_FRAME_FMT_YUV + 16), /* YYYY... UVUVUVUV...      */
+    MPP_FMT_AYUV2BPP        = (MPP_FRAME_FMT_YUV + 17), /* YYYY... UVUVUVUV...      */
     MPP_FMT_YUV_BUTT,
 
     MPP_FMT_RGB565          = (MPP_FRAME_FMT_RGB + 0),  /* 16-bit RGB               */
@@ -237,6 +239,8 @@ typedef enum {
     MPP_FMT_ABGR8888        = (MPP_FRAME_FMT_RGB + 11), /* 32-bit RGB               */
     MPP_FMT_BGRA8888        = (MPP_FRAME_FMT_RGB + 12), /* 32-bit RGB               */
     MPP_FMT_RGBA8888        = (MPP_FRAME_FMT_RGB + 13), /* 32-bit RGB               */
+    MPP_FMT_ARGB4444        = (MPP_FRAME_FMT_RGB + 14), /* 16-bit RGB               */
+    MPP_FMT_ARGB1555        = (MPP_FRAME_FMT_RGB + 15), /* 2-bit RGB                */
     MPP_FMT_RGB_BUTT,
 
     MPP_FMT_BUTT,
